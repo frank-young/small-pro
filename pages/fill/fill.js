9c58ae57a8137f0cf6f86a954dede3c930e89f08
frank <yangjunalns@qq.com>
2017-04-03 19:19:19 +0800

    联动菜单

--- pages/fill/fill.js
+++ pages/fill/fill.js
@@ -37,7 +37,7 @@ Page({
         userInfo:userInfo
       })
     })
-    
+
   },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,27 @@
 //index.js
+import City from '../../utils/city.js'
+
 Page({
   data: {
     motto: 'index',
+    date: "1996-01-01",
+    share: {
+      title: '加入我们',
+      desc: '这是一些简单的描述～',
+      path: '/pages/logs/logs'
+    },
+    sex: ['男', '女'],
+    sexIndex: 0,
+    provinceIndex:0,
+    cityIndex:0,
+    provinceArray : City.province,
+    cityArray  : City.city
   },
-  test: function() {
+  test () {
     wx.showModal({
       title: '提示',
       content: '这是一个模态弹窗',
-      success: function(res) {
+      success (res) {
         if (res.confirm) {
           console.log('用户点击确定')
         } else if (res.cancel) {
@@ -16,33 +30,54 @@ Page({
       }
     })
   },
-  onLoad: function () {
-    
+  bindSexChange (e) {
+    this.setData({
+        sexIndex: e.detail.value
+    })
   },
-  onReady:function(){
+  bindDateChange (e) {
+    this.setData({
+        date: e.detail.value
+    })
+  },
+  bindProvinceChange (e) {
+    this.setData({
+        provinceIndex: e.detail.value,
+        cityIndex: 0
+    })
+  },
+  bindCityChange (e) {
+    this.setData({
+        cityIndex: e.detail.value
+    })
+  },
+  onLoad () {
+
+  },
+  onReady (){
     // 生命周期函数--监听页面初次渲染完成
   },
-  onShow:function(){
+  onShow (){
     // 生命周期函数--监听页面显示
   },
-  onHide:function(){
+  onHide (){
     // 生命周期函数--监听页面隐藏
   },
-  onUnload:function(){
+  onUnload (){
     // 生命周期函数--监听页面卸载
   },
-  onPullDownRefresh: function() {
+  onPullDownRefresh () {
     // 页面相关事件处理函数--监听用户下拉动作
   },
-  onReachBottom: function() {
+  onReachBottom () {
     // 页面上拉触底事件的处理函数
   },
-  onShareAppMessage: function() {
+  onShareAppMessage () {
     // 用户点击右上角分享
     return {
       title: this.data.share.title,
       path: this.data.share.path,
-      success: function(res) {
+      success (res) {
         wx.showToast({
           title: '分享成功',
           icon: 'success',
@@ -53,7 +88,7 @@ Page({
           url: '../success/success'
         })
       },
-      fail: function(res) {
+      fail (res) {
         // 分享失败
         wx.showToast({
           title: '分享失败',
