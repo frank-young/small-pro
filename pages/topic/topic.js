4c58a759e7d2bf9c614e539975e7c1221289121f
frank <yangjunalns@qq.com>
2017-05-18 02:24:41 +0800

    话题刷新加载

--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -1,13 +1,73 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    src: '../../resources/topic.jpg',
+    topics: [
+      {
+        id: 1,
+        src: '../../resources/topic.jpg',
+        title: '如果在有一天',
+        desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。'
+      },
+      {
+        id: 2,
+        src: '../../resources/topic.jpg',
+        title: '如果在有一天',
+        desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。'
+      },
+      {
+        id: 3,
+        src: '../../resources/topic.jpg',
+        title: '如果在有一天',
+        desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。'
+      },
+      {
+        id: 4,
+        src: '../../resources/topic.jpg',
+        title: '如果在有一天',
+        desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。'
+      }
+    ],
+    isLoading: false
   },
-  bindViewTap: function() {
+  onLoad () {
+
   },
-  onLoad: function () {
+  onPullDownRefresh (){
+    setTimeout(() => {
+      let arr = [{
+        id: 5,
+        src: '../../resources/topic.jpg',
+        title: '刷新的数据',
+        desc: '这是刷新的数据00'
+      }]
+      let newTopics = arr.concat(this.data.topics)
+      // wx.showToast({
+      //   title: '刷新成功',
+      //   icon: 'success',
+      //   duration: 1000
+      // })
+      this.setData({
+        topics: newTopics
+      })
+      wx.stopPullDownRefresh()
+    }, 800)
+  },
+  onReachBottom () {
+    this.setData({
+      isLoading: false
+    })
+    setTimeout(() => {
+      let arr = [{
+        id: 5,
+        src: '../../resources/topic.jpg',
+        title: '加载的数据',
+        desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。'
+      }]
+      let newTopics = this.data.topics.concat(arr)
+      this.setData({
+        isLoading: true,
+        topics: newTopics
+      })
+    }, 500)
 
   }
 })
