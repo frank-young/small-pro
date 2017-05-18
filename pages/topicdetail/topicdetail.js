a9f9183d8b3686accd302151e72d4bb5673920be
frank <yangjunalns@qq.com>
2017-05-18 23:08:48 +0800

    评论

--- pages/topicdetail/topicdetail.js
+++ pages/topicdetail/topicdetail.js
@@ -6,7 +6,42 @@ Page({
       title: '如果在有一天',
       desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。悲伤的日子将会过去，快乐的日子将会来临。'
     },
-    isLoading: true
+    isLoading: true,
+    comments: [
+      {
+        id: 1,
+        topic_id: 1,
+        user_id: 1,
+        link_num: 232,
+        praise_status: 0, // 未点赞
+        nick_name: '野山椒鸡杂',
+        data_time: '5-18 13:13',
+        content: '假如生活欺骗了你',
+        replay_num: '23'
+      },
+      {
+        id: 2,
+        topic_id: 1,
+        user_id: 2,
+        link_num: 133,
+        praise_status: 1,
+        nick_name: '左右手',
+        data_time: '5-18 18:19',
+        content: '人生就是如此，会经历很多人，很多事，到最后，你会发现谁还在。',
+        replay_num: '123'
+      },
+      {
+        id: 3,
+        topic_id: 1,
+        user_id: 3,
+        link_num: 67,
+        praise_status: 0,
+        nick_name: '小文',
+        data_time: '5-18 23:33',
+        content: '人生就是如此，会经历很多人，很多事，到最后，你会发现谁还在。',
+        replay_num: '343'
+      }
+    ]
   },
   onLoad () {
 
@@ -31,5 +66,33 @@ Page({
         isLoading: true
       })
     }, 500)
+  },
+  moreCtrl () {
+    wx.showActionSheet({
+      itemList: ['回复', '举报'],
+      success: function(res) {
+        console.log(res.tapIndex)
+        if (res.tapIndex === 0) {
+          wx.navigateTo({
+            url: '../replay/replay'
+          })
+        }
+      },
+      fail: function(res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+  praiseCtrl () {
+    console.log('点赞')
+  },
+  cancelPraiseCtrl () {
+    console.log('取消点赞')
+  },
+  commentCtrl () {
+    console.log('评论操作')
+  },
+  viewReplayCtrl () {
+    console.log('查看回复操作')
   }
 })
