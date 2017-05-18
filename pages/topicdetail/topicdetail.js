709a5757abafc507d1f89341e9d013b6d7487b16
frank <yangjunalns@qq.com>
2017-05-18 18:09:46 +0800

    话题评论

--- pages/topicdetail/topicdetail.js
+++ pages/topicdetail/topicdetail.js
@@ -1,90 +1,35 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: '每天都有好心情',
-    userInfo: {},
-    share: {
-      title: '这是标题',
-      desc: '这是一些描述',
-      path: '/pages/index/index'
+    topic: {
+      id: 1,
+      src: '../../resources/topic.jpg',
+      title: '如果在有一天',
+      desc: '假如生活欺骗了你，不要悲伤，不要犹豫，不要心计。悲伤的日子将会过去，快乐的日子将会来临。'
     },
-    array: [
-        {
-          'age': 90,
-          'message': 'foo'
-        },
-        {
-          'age': 80,
-          'message': 'save'
-        },
-        {
-          'age': 45,
-          'message': 'sdahdsak'
-        }
-      ],
-      imgUrls: [
-        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-      ]
+    isLoading: true
   },
-  bindViewTap: function() {
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  onLoad () {
 
   },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
+  onPullDownRefresh (){
+    setTimeout(() => {
+      wx.showToast({
+        title: '刷新成功',
+        icon: 'success',
+        duration: 1000
+      })
+
+      wx.stopPullDownRefresh()
+    }, 800)
   },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-  },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-  },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-  },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-  },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
-  },
-  onShareAppMessage: function() {
-    // 用户点击右上角分享
-    return {
-      title: this.data.share.title,
-      path: this.data.share.path,
-      success: function(res) {
-        wx.showToast({
-          title: '分享成功',
-          icon: 'success',
-          duration: 2000
-        })
-        console.log('success')
-        wx.navigateTo({
-          url: '../success/success'
-        })
-      },
-      fail: function(res) {
-        // 分享失败
-        wx.showToast({
-          title: '分享失败',
-          duration: 2000
-        })
-      }
-    }
+  onReachBottom () {
+    this.setData({
+      isLoading: false
+    })
+    setTimeout(() => {
+      this.setData({
+        isLoading: true
+      })
+    }, 500)
   }
 })
