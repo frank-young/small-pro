7904b6725b8762687eec1e7d54332f46a4e5c22b
frank <yangjunalns@qq.com>
2017-06-04 03:09:39 +0800

    .

--- pages/applyroomslist/applyroomslist.js
+++ pages/applyroomslist/applyroomslist.js
@@ -4,15 +4,53 @@ Page({
   data: {
     offset: 0,
     limit: 10,
-    applyRooms: []
+    applyRooms: [],
+    isLoading: true
   },
   bindViewTap: function() {
   },
   onLoad () {
-    this.getApplyRoomslist(0, 10)
+    this.refreshApplyRoomslist(0, 10)
   },
+  /*
+   *  获取列表操作
+   */
   getApplyRoomslist (offset, limit) {
     let that = this
+    that.setData({
+      isLoading: false
+    })
+    wx.request({
+      url: Config.host + 'applyroom/index',
+      data: {
+        session_key: wx.getStorageSync('session_key'),
+        offset: offset,
+        limit: limit
+      },
+      method: 'POST',
+      header: {'content-type':'application/x-www-form-urlencoded'},
+      success (res){
+        if (res.data.success) {
+          let applyRooms = []
+          applyRooms = that.data.applyRooms
+          applyRooms.push(...res.data.bizContent)
+          that.setData({
+            applyRooms: applyRooms,
+            isLoading: true,
+            offset: offset + limit
+          })
+        }
+      }
+    })
+  },
+  /*
+   *  刷新操作
+   */
+  refreshApplyRoomslist (offset, limit) {
+    let that = this
+    that.setData({
+      isLoading: false
+    })
     wx.request({
       url: Config.host + 'applyroom/index',
       data: {
@@ -32,5 +70,71 @@ Page({
         }
       }
     })
-  }
+  },
+  /*
+   *  同意操作
+   */
+  agree (event) {
+    let that = this
+    let openid = event.target.dataset.openid
+    wx.showLoading({
+      title: '操作中'
+    })
+    wx.request({
+      url: Config.host + 'applyroom/agree',
+      data: {
+        session_key: wx.getStorageSync('session_key'),
+        openid
+      },
+      method: 'POST',
+      header: {'content-type':'application/x-www-form-urlencoded'},
+      success (res){
+        if (res.data.success) {
+          that.refreshApplyRoomslist(0, 10)
+          wx.showToast({
+            title: '已同意',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+  /*
+   *  拒绝操作
+   */
+  notAgree (event) {
+    let that = this
+    let openid = event.target.dataset.openid
+    wx.showLoading({
+      title: '操作中'
+    })
+    wx.request({
+      url: Config.host + 'applyroom/notagree',
+      data: {
+        session_key: wx.getStorageSync('session_key'),
+        openid
+      },
+      method: 'POST',
+      header: {'content-type':'application/x-www-form-urlencoded'},
+      success (res){
+        if (res.data.success) {
+          that.refreshApplyRoomslist(0, 10)
+          wx.showToast({
+            title: '已拒绝',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+  onPullDownRefresh (){
+    this.refreshApplyRoomslist(0, 10)
+    wx.stopPullDownRefresh()
+  },
+  onReachBottom () {
+    this.setData({
+      isLoading: false
+    })
+    this.getApplyRoomslist(this.data.offset, this.data.limit)
+  },
 })
